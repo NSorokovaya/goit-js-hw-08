cc9a24d7dac9eb4486b4eb8b7ac217f301706f85
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-01-31 10:09:16 -0500

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = "videoplayer-current-time";
+
+
+player.on('timeupdate', throttle (function(time) {
+    localStorage.setItem('videoplayer-current-time', time.seconds);
+}, 1000)
+)
+
+
+
+  player.setCurrentTime(localStorage.getItem(KEY)||0);
 
